186628b9089ae16d13d69ae82201875af70dfdf4
Glebovski <andrewglebovski@gmail.com>
2022-09-27 22:40:57 +0300

    Add hash check

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,17 @@ int main() {
 
     stack_constructor(&stack, 10);
 
-    for(int i = 1; i <= 11; i++)
+    for(int i = 1; i <= 12; i++)
         stack_push(&stack, i);
 
     for(int i = 1; i <= 8; i++) {
         Object value = 0;
         stack_pop(&stack, &value);
     }
+
+    stack_dump(&stack, stack_check(&stack));
+
+    stack.capacity = 15;
 
     stack_dump(&stack, stack_check(&stack));
 
--- src/stack.hpp
+++ src/stack.hpp
@@ -72,28 +72,37 @@ typedef int Object;
 typedef long long StackSize;
 typedef unsigned int ErrorBits;
 typedef unsigned long long CanaryType;
+typedef unsigned long long HashType;
 
 
 typedef struct {
     CanaryType canary_begin = CANARY_VALUE;
+
     Object *data = NULL;
     StackSize size = 0;
     StackSize capacity = 0;
+
+    HashType struct_hash = 0;
+    HashType buffer_hash = 0;
+
     CanaryType canary_end = CANARY_VALUE;
 } Stack;
 
 
 enum ERROR_BIT_FLAGS {
-    STACK_OK         =   0, ///< Stack is ok
-    NULL_DATA        =   1, ///< Stack data points to NULL
-    INVALID_SIZE     =   2, ///< Size is larger than capacity or negative
-    INVALID_CAPACITY =   4, ///< Capacity is larger than type's maximum or negative
-    UNEXP_POISON_VAL =   8, ///< Unexpected poison value before size
-    UNEXP_NORMAL_VAL =  16, ///< Unexpected normal value after size
-    INVALID_ARGUMENT =  32, ///< Invalid argument given to the function
-    EMPTY_STACK      =  64, ///< No elements to pop
-    ALLOCATE_FAIL    = 128, ///< Memory allocate return NULL
-    CANARY_ERROR     = 256  ///< Wrong canary value detected
+    STACK_OK         =    0, ///< Stack is ok
+    NULL_DATA        =    1, ///< Stack data points to NULL
+    INVALID_SIZE     =    2, ///< Size is larger than capacity or negative
+    INVALID_CAPACITY =    4, ///< Capacity is larger than type's maximum or negative
+    UNEXP_POISON_VAL =    8, ///< Unexpected poison value before size
+    UNEXP_NORMAL_VAL =   16, ///< Unexpected normal value after size
+    INVALID_ARGUMENT =   32, ///< Invalid argument given to the function
+    EMPTY_STACK      =   64, ///< No elements to pop
+    ALLOCATE_FAIL    =  128, ///< Memory allocate return NULL
+    STRUCT_CANARY    =  256, ///< Stack canary value was overwritten
+    BUFFER_CANARY    =  512, ///< Buffer canary value was overwritten
+    BUFFER_HASH_FAIL = 1024, ///< Wrong buffer hash sum
+    STRUCT_HASH_FAIL = 2048, ///< Wrong stack hash sum
 };
 
 
@@ -124,6 +133,15 @@ void print_errors(ErrorBits error);
 void print_binary(ErrorBits n);
 
 
+void set_hash(Stack *stack);
+
+
+ErrorBits check_struct_hash(Stack *stack);
+
+
+HashType gnu_hash(void *ptr, size_t size);
+
+
 
 
 ErrorBits stack_constructor(Stack *stack, StackSize capacity) {
@@ -141,14 +159,21 @@ ErrorBits stack_constructor(Stack *stack, StackSize capacity) {
     stack -> capacity = capacity;
     stack -> size = 0;
 
+    set_hash(stack);
+
+    stack -> canary_begin = CANARY_VALUE;
+    stack -> canary_end = CANARY_VALUE;
+
     return ERROR_BIT_FLAGS::STACK_OK;
 }
 
 
 ErrorBits stack_resize(Stack *stack, StackSize capacity) {
-    ErrorBits error = stack_check(stack);
+    CHECK(stack, return ERROR_BIT_FLAGS::INVALID_ARGUMENT);
+    CHECK(capacity >= 10, return ERROR_BIT_FLAGS::INVALID_ARGUMENT);
 
-    if (error) return error;
+    ErrorBits error = stack_check(stack);
+    CHECK(!error, return error);
 
     char *true_pointer = ((char *)(stack -> data)) - sizeof(CanaryType);
     true_pointer = (char *) realloc(true_pointer, capacity * sizeof(Object) + 2 * sizeof(CanaryType));
@@ -163,36 +188,44 @@ ErrorBits stack_resize(Stack *stack, StackSize capacity) {
     
     stack -> capacity = capacity;
 
+    set_hash(stack);
+
     return error;
 }
 
 
 ErrorBits stack_push(Stack *stack, Object object) {
-    ErrorBits error = stack_check(stack);
+    CHECK(stack, return ERROR_BIT_FLAGS::INVALID_ARGUMENT);
 
-    if (error) return error;
+    ErrorBits error = stack_check(stack);
+    CHECK(!error, return error);
 
     if ((stack -> size) + 1 > stack -> capacity)
         error = stack_resize(stack, (StackSize) ((float)(stack -> capacity) * STACK_FACTOR));
 
-    if (error) return error;
+    CHECK(!error, return error);
 
     (stack -> data)[(stack -> size)++] = object;
+
+    set_hash(stack);
 
     return error;
 }
 
 
 ErrorBits stack_pop(Stack *stack, Object *object) {
-    ErrorBits error = stack_check(stack);
-
-    if (error) return error;
-    if (stack -> size == 0) return ERROR_BIT_FLAGS::EMPTY_STACK;
-
+    CHECK(stack, return ERROR_BIT_FLAGS::INVALID_ARGUMENT);
     CHECK(object, return ERROR_BIT_FLAGS::INVALID_ARGUMENT);
+
+    ErrorBits error = stack_check(stack);
+    CHECK(!error, return error);
+
+    CHECK(stack -> size, return ERROR_BIT_FLAGS::EMPTY_STACK);
 
     *object = (stack -> data)[--(stack -> size)];
     (stack -> data)[(stack -> size)] = POISON_VALUE;
+
+    set_hash(stack);
 
     if (((stack -> size) < ((stack -> capacity) / (StackSize)(2 * STACK_FACTOR))) && stack -> capacity > 10)
         error = stack_resize(stack, (stack -> capacity) / (StackSize)(STACK_FACTOR));
@@ -203,8 +236,7 @@ ErrorBits stack_pop(Stack *stack, Object *object) {
 
 ErrorBits stack_destructor(Stack *stack) {
     ErrorBits error = stack_check(stack);
-
-    if (error) return error;
+    CHECK(!error, return error);
 
     free(stack -> data);
     stack -> data = NULL;
@@ -221,14 +253,14 @@ ErrorBits stack_check(Stack *stack) {
 
     CHECK(stack, return ERROR_BIT_FLAGS::INVALID_ARGUMENT);
 
-    // printf("%lld %lld\n", stack -> canary_begin, stack ->canary_end);
+    CHECK(stack -> canary_begin == CANARY_VALUE && stack -> canary_end == CANARY_VALUE, return ERROR_BIT_FLAGS::STRUCT_CANARY);
 
-    CHECK(stack -> canary_begin == CANARY_VALUE && stack -> canary_end == CANARY_VALUE, return ERROR_BIT_FLAGS::CANARY_ERROR);
+    CHECK(!check_struct_hash(stack), return ERROR_BIT_FLAGS::STRUCT_HASH_FAIL);
 
     char *true_pointer = ((char *)(stack -> data)) - sizeof(CanaryType); // pointer to the real buffer start
 
-    CHECK(*(CanaryType *)(true_pointer) == CANARY_VALUE, return ERROR_BIT_FLAGS::CANARY_ERROR);
-    CHECK(*(CanaryType *)(true_pointer + sizeof(CanaryType) + stack -> capacity * sizeof(Object)) == CANARY_VALUE, return ERROR_BIT_FLAGS::CANARY_ERROR);
+    CHECK(*(CanaryType *)(true_pointer) == CANARY_VALUE,                                                           return ERROR_BIT_FLAGS::BUFFER_CANARY);
+    CHECK(*(CanaryType *)(true_pointer + sizeof(CanaryType) + stack -> capacity * sizeof(Object)) == CANARY_VALUE, return ERROR_BIT_FLAGS::BUFFER_CANARY);
 
     CHECK(stack -> capacity >= 0 && stack -> capacity <= MAX_CAPACITY_VALUE, error += ERROR_BIT_FLAGS::INVALID_CAPACITY);
 
@@ -236,7 +268,9 @@ ErrorBits stack_check(Stack *stack) {
 
     CHECK(stack -> data, error += ERROR_BIT_FLAGS::NULL_DATA; return error);
 
-    if (HAS_ERROR(error, ERROR_BIT_FLAGS::INVALID_SIZE) || HAS_ERROR(error, ERROR_BIT_FLAGS::INVALID_CAPACITY))
+    CHECK(gnu_hash(stack -> data, stack -> size * sizeof(Object)) == stack -> buffer_hash, error += ERROR_BIT_FLAGS::BUFFER_HASH_FAIL);
+
+    if (HAS_ERROR(error, ERROR_BIT_FLAGS::INVALID_SIZE) || HAS_ERROR(error, ERROR_BIT_FLAGS::INVALID_CAPACITY) || HAS_ERROR(error, ERROR_BIT_FLAGS::STRUCT_HASH_FAIL))
         return error;
 
     for(StackSize i = 0; i < stack -> capacity; i++) {
@@ -257,9 +291,12 @@ void stack_dump(Stack *stack, ErrorBits error) {
 
     print_errors(error);
 
-    printf("Capacity: %lld\nSize: %lld \nData[%p]:\n", stack -> capacity, stack -> size, stack -> data);
+    printf("Capacity: %llu\nSize: %llu\nBuffer hash: %llu\nStruct hash: %llu\nData[%p]:\n", 
+            stack -> capacity, stack -> size, stack -> buffer_hash, stack -> struct_hash, stack -> data);
     
-    if (HAS_ERROR(error, ERROR_BIT_FLAGS::NULL_DATA) || HAS_ERROR(error, ERROR_BIT_FLAGS::INVALID_CAPACITY) || HAS_ERROR(error, ERROR_BIT_FLAGS::NULL_DATA)) return;
+    if (HAS_ERROR(error, ERROR_BIT_FLAGS::NULL_DATA) || HAS_ERROR(error, ERROR_BIT_FLAGS::INVALID_CAPACITY) 
+            || HAS_ERROR(error, ERROR_BIT_FLAGS::STRUCT_HASH_FAIL) || HAS_ERROR(error, ERROR_BIT_FLAGS::STRUCT_CANARY)) 
+        return;
 
     for(StackSize i = 0; i < stack -> capacity; i++) {
         printf("    [%lld]", i); // object index
@@ -291,10 +328,13 @@ void print_errors(ErrorBits error) {
     if (HAS_ERROR(error, ERROR_BIT_FLAGS::NULL_DATA))        printf("Stack has NULL data\n");
     if (HAS_ERROR(error, ERROR_BIT_FLAGS::UNEXP_NORMAL_VAL)) printf("Unexpected normal value\n");
     if (HAS_ERROR(error, ERROR_BIT_FLAGS::UNEXP_POISON_VAL)) printf("Unexpected poison value\n");
-    if (HAS_ERROR(error, ERROR_BIT_FLAGS::CANARY_ERROR))     printf("Wrong canary value\n");
+    if (HAS_ERROR(error, ERROR_BIT_FLAGS::STRUCT_CANARY))    printf("Wrong struct canary\n");
+    if (HAS_ERROR(error, ERROR_BIT_FLAGS::BUFFER_CANARY))    printf("Wrong buffer canary\n");
     if (HAS_ERROR(error, ERROR_BIT_FLAGS::EMPTY_STACK))      printf("Pop empty stack\n");
     if (HAS_ERROR(error, ERROR_BIT_FLAGS::INVALID_ARGUMENT)) printf("Invalid argument given to the function\n");
     if (HAS_ERROR(error, ERROR_BIT_FLAGS::ALLOCATE_FAIL))    printf("Failed to allocate memory\n");
+    if (HAS_ERROR(error, ERROR_BIT_FLAGS::BUFFER_HASH_FAIL)) printf("Wrong buffer hash\n");
+    if (HAS_ERROR(error, ERROR_BIT_FLAGS::STRUCT_HASH_FAIL)) printf("Wrong struct hash\n");
 }
 
 
@@ -304,4 +344,44 @@ void print_binary(ErrorBits n) {
         putchar(((n & k) > 0) + '0');
         k = k >> 1;
     }
+}
+
+
+ErrorBits check_struct_hash(Stack *stack) {
+    CHECK(stack, return ERROR_BIT_FLAGS::INVALID_ARGUMENT);
+
+    ErrorBits error = ERROR_BIT_FLAGS::STACK_OK;
+
+    HashType h1 = stack -> struct_hash, h2 = stack -> buffer_hash;
+
+    stack -> struct_hash = 0;
+    stack -> buffer_hash = 0;
+
+    CHECK(gnu_hash(stack, sizeof(Stack)) == h1, error += ERROR_BIT_FLAGS::STRUCT_HASH_FAIL);
+
+    stack -> struct_hash = h1;
+    stack -> buffer_hash = h2;
+
+    return error;
+}
+
+
+void set_hash(Stack *stack) {
+    CHECK(stack, return);
+
+    stack -> struct_hash = 0;
+    stack -> buffer_hash = 0;
+
+    stack -> struct_hash = gnu_hash(stack, sizeof(Stack));
+    stack -> buffer_hash = gnu_hash(stack -> data, stack -> size * sizeof(Object));
+}
+
+
+HashType gnu_hash(void *ptr, size_t size) {
+    HashType hash = 5381;
+
+    for(size_t i = 0; i < size; i++)
+        hash = hash * 33 + ((char *)(ptr))[i];
+
+    return hash;
 }
